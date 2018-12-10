598c10941ddc5f27b845c34458f1ac146e13fed3
ArielFeingold <feingold.ariel@gmail.com>
2018-12-09 22:31:22 -0500

    installed material ui, redux and react-router

--- client/src/index.js
+++ client/src/index.js
@@ -2,11 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
+const composeEnhancers = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null || compose;
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+import authReducer from './store/reducers/authReducer';
+
+const rootReducer = combineReducers({
+  auth: authReducer,
+});
+
+const store = createStore(rootReducer, composeEnhancers(
+    applyMiddleware(thunk)
+));
+
+ReactDOM.render(
+    <Provider store={store}>
+      <BrowserRouter >
+          <App />
+      </BrowserRouter>
+    </Provider>,
+document.getElementById('root'));
