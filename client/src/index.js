9c77a0dc9f369ab7d0caf25b0f2ceb0a6b07a686
ArielFeingold <feingold.ariel@gmail.com>
2018-12-10 06:57:57 -0500

    bug fixes

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+
 import './index.css';
 import App from './App';
 
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
-const composeEnhancers = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null || compose;
+import thunk from 'redux-thunk';
 
 import authReducer from './store/reducers/authReducer';
 
+const composeEnhancers = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null || compose;
 const rootReducer = combineReducers({
   auth: authReducer,
 });
